a47d57146f5943343a06c00e345b759120dd41a9
Niraj Dudani <niraj@locus.sh>
2019-02-11 16:06:58 +0530

    Added 'dummy-lein-project', generated by "lein new app dummy-lein-project".

---
+++ dummy-lein-project/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dummy-lein-project
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ dummy-lein-project/test/dummy_lein_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns dummy-lein-project.core-test
+  (:require [clojure.test :refer :all]
+            [dummy-lein-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
